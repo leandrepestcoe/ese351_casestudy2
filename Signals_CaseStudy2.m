b26f1bb533e1011998850cdc404114b3d2575897
leandrepestcoe <54565087+leandrepestcoe@users.noreply.github.com>
2021-04-21 00:26:20 -0500

    made progress

--- Signals_CaseStudy2.m
+++ Signals_CaseStudy2.m
@@ -3,7 +3,7 @@
 % ESE 351: Signals and Systems
 % Created on: 4/20/21, Last Edited on: 4/20/21
 
-%% Define pulse shape
+%% Define Pulse Shape p(t)
 Ts = 0.1; %symbol period (rate 1/Ts)
 dt = 0.01; %sample period
 w = 5*Ts; %width
@@ -11,38 +11,74 @@ t = -w:dt:w; %time vector
 fs = 1/dt; %sample frequency
 
 %sinc
-%sinc = sinc(t/Ts); %define sinc
+sinc_signal = sinc(t/Ts); %define sinc
 
 %triangular pulse
-triang = tripuls(t,w*2);
-
-%rectangular pulse
-rect = zeros(1,length(t));
-for i=(1:length(t)/2)
-    rect(i)=1;
-end
+triang_signal = tripuls(t,w*2);
 
 %decide which signal to use for simulation
-signal = rect;
+p_t = sinc_signal;
 
-figure, plot(t,signal);
-xlabel('time (s)'), ylabel('x(t)'), title('Truncated Signal')
+figure, plot(t,p_t);
+xlabel('time (s)'), ylabel('p(t)'), title('Truncated Signal p(t)')
 
-%% Signal Modulation
+%% Nyquist Criteria
+
+Nfft = 1024; %length of fft
+f1 = (-fs/2:fs/Nfft:(fs/2));
+f2 = (fs-2*pi/Ts:fs/Nfft:fs+(2*pi/Ts));
+fft_signal = abs(fft(p_t,Nfft));
+fft_signal1 = [fft_signal((length(fft_signal)/2)+1:end), fft_signal(1:(length(fft_signal)/2))];
+
+figure
+plot(f1(1:length(fft_signal1)),fft_signal1), grid on;
+hold on
+plot(f2(1:length(fft_signal1)),fft_signal1), grid on;
+xlabel('frequency (Hz)'), ylabel('|Y(j\omega)|');
+
+%% Noise-free PAM Signal y(t)
+N = 10;
+bits = 2*((rand(1,N)>0.5)-0.5);
+
+x_t = zeros(1,length(t));
+for i=1:5:length(t)
+    for j=1:length(bits)
+        x_t(i)=bits(j);
+    end
+end
+y_t = conv(x_t,p_t);
+y_t = y_t(1:length(p_t));
+figure();
+plot(t,y_t(1:length(t)));
+title('Transmitted Signal y(t)');
+xlabel('Time[s]');
+ylabel('Amplitude');
+
+%% Signal Modulation (Up-Conversion)
 wc = 2*pi*20; %20 Hz modulation
-mod_signal = signal.*cos(wc*t);
+mod_signal = y_t.*cos(wc*t);
 
 figure
 subplot(2,1,1), plot(t,mod_signal), grid on;
-xlabel('time (s)'), ylabel('y(t)'), title('Modulated Signal')
+xlabel('time (s)'), ylabel('y(t)cos(wc*t)'), title('Modulated Signal')
 
 Nfft = 1024; %length of fft
 f = (0:fs/Nfft:fs-fs/Nfft);
 subplot(2,1,2), plot(f,abs(fft(mod_signal,Nfft))), grid on;
 xlabel('frequency (Hz)'), ylabel('|Y(j\omega)|');
 
-%% Signal Demodulation
-demod_signal = mod_signal.*cos(wc*t);
+%% Noisy Recieved Signal r(t)
+sigma = 0.5;
+n_t = sigma*randn(1,length(mod_signal));
+r_t = mod_signal + n_t;
+
+plot(t,r_t(1:length(t)));
+title('Received Signal r(t)');
+xlabel('Time[s]');
+ylabel('Amplitude');
+
+%% Signal Demodulation (Down-Conversion)
+demod_signal = r_t.*cos(wc*t);
 
 figure
 subplot(2,1,1), plot(t,demod_signal), grid on;
@@ -52,100 +88,19 @@ f = (0:fs/Nfft:fs-fs/Nfft);
 subplot(2,1,2), plot(f,abs(fft(demod_signal,Nfft))), grid on;
 xlabel('frequency (Hz)'), ylabel('|X_r(j\omega)|')
 
-%%
-% c) binary message 
-N = 10;
-x_n = 2*((rand(1,N)>0.5)-0.5);
+%% Matched Filter Receiver
 
-% d) y(t)
-t_p = 0.1;
-time = -t_p:1/fs:((N-1)*2*t_p)+t_p;
-x_t = zeros(1,length(time));
-i = 1;
-for j=11:10:length(time);
-    x_t(j)=x_n(i);
-    i = i+1;
-    if i==N+1
-        break 
+xn_tilda = ones(1,length(t));
+p_neg = p_t(end:-1:1);
+z_t = conv(r_t,p_neg);
+for i=1:length(z_t)
+    if z_t(i)<=0
+        xn_tilda(i)=-1;
     end
 end
-y_t = conv(x_t,p_t);
-y_t_new = y_t(11:211);
 
-% e) r(t) noise
-sigma = 0.5;
-n_t = sigma*randn(1,length(y_t_new));
-r_t = y_t_new + n_t;
+plot(t,xn_tilda(1:length(t)));
 
-% f) Plots
-% i) Pulse shape of p(t), and spectrum, P(w)
-figure, plot(t,p_t), grid on, title('p(t) Pulse Shape'), xlabel('Time'), ylabel('Amplitude');
-figure, plot(t,fft(p_t)), grid on, title('P(w)'), xlabel('Time'), ylabel('Amplitude');
-
-% ii) Noise-free transmitted signal, y(t)
-figure, plot(time,y_t_new), grid on, title('y(t), Noise-free Transmitted Signal'),...
-    xlabel('Time'), ylabel('Amplitude');
+%% Noise Levels and Error Rates
 
 
-% iii) Noisy received signal, r(t)
-figure, plot(time,r_t), grid on, title('r(t), Noisy Received Signal'), xlabel('Time'), ylabel('Amplitude'), legend('Sigma=0.5, Bit Rate= 1/2*T_p');
-
-% iv) Sent message, x_n, and decoded message, x_hat_n
-% 1) Sign-based receiver 
-i = 1;
-for l=11:10:201
-    if r_t(l) <= 0;
-        x_n_received(i) = -1;
-        i = i + 1;
-    end
-    if r_t(l) > 0;
-        x_n_received(i) = 1;
-        i = i + 1;
-    end
-    if i==N+1
-        break 
-    end
-end
-figure, hold on, grid on, plot(x_n,'--o'), plot(x_n_received), legend('x_n', 'x_n__received'), text(3.5,0.5,'Sigma=0.5, Bit Rate= 1/2*T_p, Error Rate = 0'),...
-     title('Sign-based Receiver'),xlabel('bit number'),ylabel('bit value');
-
-% error rate for sign-based receiver
-correct = 0;
-for k=1:10
-    if x_n(k)==x_n_received(k)
-        correct=correct+1;
-    end
-end
-error_signbased = (10-correct)/10;
-
-% 2) Matched Filter
-
-% z_t
-z_t = conv(r_t,p_t);
-z_t_new = z_t(11:211);
-i = 1;
-for l=11:10:201
-    if z_t_new(l) <= 0;
-        x_n_received2(i) = -1;
-        i = i + 1;
-    end
-    if z_t_new(l) > 0;
-        x_n_received2(i) = 1;
-        i = i + 1;
-    end
-    if i==N+1
-        break 
-    end
-end
-figure, hold on, grid on, plot(x_n,'--o'), plot(x_n_received2), legend('x_n', 'x_n__received2'), text(3.5,0.5,'Sigma=0.5, Bit Rate= 1/2*T_p, Error Rate = 0'),...
-    title('Matched Filter'),xlabel('bit number'),ylabel('bit value');
-
-
-% error rate for matched filter
-correct = 0;
-for k=1:10
-    if x_n(k)==x_n_received2(k)
-        correct=correct+1;
-    end
-end
-error_matched = (10-correct)/10;
