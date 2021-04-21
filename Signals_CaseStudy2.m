286bbf2c755f3e7455d589cb6dc9ac0da674f6e2
leandrepestcoe <54565087+leandrepestcoe@users.noreply.github.com>
2021-04-21 14:59:43 -0500

    fixed y_t

--- Signals_CaseStudy2.m
+++ Signals_CaseStudy2.m
@@ -1,7 +1,7 @@
 % Case Study 2
 % Leandre Pestcoe and Julianne Wegmann
 % ESE 351: Signals and Systems
-% Created on: 4/20/21, Last Edited on: 4/20/21
+% Created on: 4/20/21, Last Edited on: 4/21/21
 
 %% Define Pulse Shape p(t)
 Ts = 0.1; %symbol period (rate 1/Ts)
@@ -11,15 +11,15 @@ t = -w:dt:w; %time vector
 fs = 1/dt; %sample frequency
 
 %sinc
-sinc_signal = sinc(t/Ts); %define sinc
+sinc_p_t = sinc(t/Ts); %define sinc
 
 %triangular pulse
-triang_signal = tripuls(t,w*2);
+triang_p_t = tripuls(t,w*2);
 
 %decide which signal to use for simulation
-p_t = sinc_signal;
+p_t = sinc_p_t;
 
-figure, plot(t,p_t);
+figure, plot(t,p_t), grid on;
 xlabel('time (s)'), ylabel('p(t)'), title('Truncated Signal p(t)')
 
 %% Nyquist Criteria
@@ -38,25 +38,30 @@ xlabel('frequency (Hz)'), ylabel('|Y(j\omega)|');
 
 %% Noise-free PAM Signal y(t)
 N = 10;
-bits = 2*((rand(1,N)>0.5)-0.5);
+bits1 = 2*((rand(1,N)>0.5)-0.5);
+bits2 = 2*((rand(1,N)>0.5)-0.5);
+bits3 = 2*((rand(1,N)>0.5)-0.5);
 
-x_t = zeros(1,length(t));
-for i=1:5:length(t)
-    for j=1:length(bits)
-        x_t(i)=bits(j);
-    end
+x_t1 = zeros(1,N*(Ts/dt));
+for i=1:length(bits1)
+    x_t1((i-1)*(Ts/dt)+1)=bits1(i);
 end
-y_t = conv(x_t,p_t);
-y_t = y_t(1:length(p_t));
+y_t1 = conv(x_t1,p_t);
+t = (0:length(y_t1)-1)*dt;
+x_new = zeros(1,length(y_t1));
+x_new((length(p_t)+1)/2:(length(x_t1)+(length(p_t)+1)/2)-1) = x_t1;
+
 figure();
-plot(t,y_t(1:length(t)));
+plot(t,y_t1), grid on;
+hold on
+stem(t,x_new);
 title('Transmitted Signal y(t)');
 xlabel('Time[s]');
-ylabel('Amplitude');
+ylabel('y(t)');
 
 %% Signal Modulation (Up-Conversion)
-wc = 2*pi*20; %20 Hz modulation
-mod_signal = y_t.*cos(wc*t);
+wc20 = 2*pi*20; %20 Hz modulation
+mod_signal20 = y_t1.*cos(wc*t);
 
 figure
 subplot(2,1,1), plot(t,mod_signal), grid on;
@@ -99,7 +104,8 @@ for i=1:length(z_t)
     end
 end
 
-plot(t,xn_tilda(1:length(t)));
+%plot(t,xn_tilda(1:length(t)));
+plot(z_t);
 
 %% Noise Levels and Error Rates
 
